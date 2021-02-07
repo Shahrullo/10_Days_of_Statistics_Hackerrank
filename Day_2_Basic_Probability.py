ef079e8cd446fffca7514d857864caf43ca87306
Shahrullo <shahrullo1995@mail.ru>
2021-02-08 07:34:34 +0900

    Day 2 three problems solutions added

---
+++ Day_2_Basic_Probability.py
@@ -0,0 +1,14 @@
+"""
+Objective
+In this challenge, we practice calculating probability. Check out the Tutorial tab for a breakdown of probability fundamentals!
+
+
+Task
+In a single toss of  fair (evenly-weighted) six-sided dice, find the probability that their sum will be at most .
+
+A) <p>2 / 3</p>
+B) <p>5 / 6</p>   ------ True
+C) <p>1 / 4</p>
+D) <p>1 / 6</p>
+
+"""
