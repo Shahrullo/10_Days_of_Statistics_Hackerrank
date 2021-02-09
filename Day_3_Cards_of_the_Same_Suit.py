8973c34c2fc610d7f61b54a8913122fe878bfb74
Shahrullo <shahrullo1995@mail.ru>
2021-02-09 09:34:53 +0900

    Day 3 answers for all questions

---
+++ Day_3_Cards_of_the_Same_Suit.py
@@ -0,0 +1,16 @@
+"""
+Objective
+
+In this challenge, we're getting started with combinations and permutations. Check out the Tutorial tab for learning materials!
+
+
+Task
+
+You draw 2 cards from a standard 52card deck without replacing them. What is the probability that both cards are of the same suit?
+
+A) 1 / 156
+B) 1 / 39
+C) 12 / 39
+D) 12 / 51 -----True
+
+"""
